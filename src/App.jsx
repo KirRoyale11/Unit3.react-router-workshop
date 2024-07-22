7d7df3d0af14c6690d86390eab1f41c5f451f8e2
Kirsti Esch <miss.k.esch@gmail.com>
2024-07-22 12:22:23 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Blue from "./components/Blue/Blue";
+import Red from "./components/Red/Red";
+
+function App() {
+  return (
+    <>
+      <div id="container">
+        {/* <h1>Hello React Router!</h1> SUCCESS */}
+        <div id="navbar">
+          <Link to="/">Home</Link>
+          <Link to="/blue">Blue</Link>
+          <Link to="/red">Red</Link>
+        </div>
+        <div id="main-section">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/blue" element={<Blue />} />
+            <Route path="/red" element={<Red />} />
+          </Routes>
+          </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
