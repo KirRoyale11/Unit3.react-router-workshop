96757a3591ff48eb249f2c206218b4f98dbdd3e3
Kirsti Esch <miss.k.esch@gmail.com>
2024-07-22 13:18:15 -0400

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Main from "./components/Main/Main";
 import Navbar from "./components/NavBar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div id="container">
         <Navbar />
         <Main />
+        <Footer />
       </div>
     </>
   );
